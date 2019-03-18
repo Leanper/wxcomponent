7e96820fb41feb6dbd79bb645a7489adc6eada22
Leanper <15116920121@163.com>
2019-03-18 15:27:33 +0800

    添加几种api

---
+++ ex/api.md
@@ -0,0 +1,30 @@
+## 系统信息 wx.getsystemInfo
+  * console.log(res.model)  型号 
+   *     console.log(res.pixelRatio) 像素比
+   *     console.log(res.windowWidth) 
+    *    console.log(res.windowHeight) 可用宽高度
+    *    console.log(res.language) 语言
+    *    console.log(res.version)  微信版本
+     *   console.log(res.platform) 客户端平台  
+
+
+
+## wx的展示信息
+  * wx.showTabBarRedDot 展示右上角红点
+    * wx.setNavigationBarTitle 动态修改标题
+    
+  *  //检测tabbar的点击事件处理 onTabItemTap(item){
+
+      console.log(item.index)
+       console.log(item.pagePath)
+      console.log(item.text)
+      console.log(app.globalData)
+  }
+    * wx.setNavigationBarTitle 动态修改标题
+    * wx.setNavigationBarTitle 动态修改标题
+    * wx.setNavigationBarTitle 动态修改标题
+    * wx.setNavigationBarTitle 动态修改标题
+
+
+
+  
--- pages/leanper/index/index.js
+++ pages/leanper/index/index.js
@@ -2,11 +2,16 @@
 //获取应用实例
 const app = getApp()
 
+App({
+  globalData: 323
+})
+
 var brand;
 var ratio;
 ischeck:false
 
 Page({
+  showView: false,
   data: {
     imgUrls: [
       '/images/aa.jpg',
@@ -29,14 +34,23 @@ Page({
       }),
       { "title": brand, "src": '/images/hh.jpg', "dec": "狍子1" },
       { "title": ratio, "src": "/images/hh.jpg", "dec": "狍子2" },
-      { "title": "标题3", "src": "/images/hh.jpg", "dec": "狍子3" }
+      { "title": "标题3", "src": "/images/hh.jpg", "dec": "狍子3" },
+
+      wx.hideShareMenu({
+     
+      })
     ],
+
    
 
   },
+
+
+
   checkboxChange(e) {
     console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   },
+
 
 
 
@@ -48,6 +62,8 @@ onReady: function () {
     //获得dialog组件
   this.dialog = this.selectComponent("#alert");
   },
+
+
   showDialog() {
     this.dialog.showDialog();
     this.dialog.getInfos("sssssssssssss")
@@ -63,7 +79,67 @@ onReady: function () {
 
     console.log(this.dialog.getInfos());
     this.dialog.hideDialog();
+
+
+
+  },
+
+  //分享按钮的点击处理
+  onShareAppMessage(res){
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return{
+      title:"铁了头",
+      path:"pages/leanper/five/five",
+      imageUrl:"/images/hh.jpg"
+    }
+  
+   
+
+  },
+   //刷新请求
+    onPullDownRefresh() {
+  console.log("333333333333333333333");
+  this.setData({
+    showView: true,
+    
+  })
+  },
+
+  //检测tabbar的点击事件处理
+  onTabItemTap(item){
+
+      console.log(item.index)
+    console.log(item.pagePath)
+    console.log(item.text)
+    console.log(app.globalData)
+
+    wx.showToast({
+      title: '抬头',
+    }),
+    //展示tabbar的
+      wx.showTabBarRedDot({
+        index: 2,
+
+      })
+
+    console.log(app.globalData.change)
+
+//动态设置导航栏标题
+    wx.setNavigationBarTitle({
+      title: '还可以这样玩',
+    }),
+      wx.showTabBar({
+        animation: true
+      })
+    
+    
+
+
   }
+
 
 
 
